6e7d7c9ca62a3cabe2996bb5403c0149a7873e86
inizayzay <zackyzalfa6@gmail.com>
2025-11-08 04:00:54 +0700

    Fix

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -134,18 +134,6 @@ export default function LoginForm() {
             )}
           </Button>
         </div>
-
-        {/* Separator */}
-        <div className="relative my-6">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-300"></div>
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-[#FBFCFF] text-gray-500">
-              Atau Masuk dengan
-            </span>
-          </div>
-        </div>
       </form>
     </div>
   );
